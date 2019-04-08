0fe8507f161acdddbd774e80ed6481de1f4f5055
VikeshDharani18sw46 <49350638+VikeshDharani18sw46@users.noreply.github.com>
2019-04-08 17:57:02 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,17 @@
+class Task1
+{
+public static  void  main(String []args)
+{
+String a="Vikesh Dharani";
+int b=500;
+double x=79.1;
+float i=(float)x;
+char d='A';
+String e="Pass";
+System.out.println("Name          :"+a);
+System.out.println("Total  Marks  :"+b);
+System.out.println("Percentage    :"+i);
+System.out.println("Grade         :"+d);
+System.out.println("Status        :"+e);
+}
+}
---
+++ Task2.java
@@ -0,0 +1,12 @@
+class Task2
+{
+public static void main(String []args)
+{
+double a =50.25;
+int    i=(int)a;
+float  f=(float)a-i;
+System.out.println("Actual Number     :"+a); 
+System.out.println("Integer Number    :"+i);
+System.out.println("Fractional Number :"+f);
+}
+}
